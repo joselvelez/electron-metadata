e9cede3f5777733652a63c33a091a414c8b8db2f
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2019-05-22 14:52:03 -0500

    Setup the browser process

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
+// declare this as a variable globablly so we can
+// reference it and so it will not be garbage collected
+let mainWindow;
+
+// wait for the main process to be ready
 app.on('ready', () => {
-    console.log('app ready');
+    // path to our html
+    const htmlPath = path.join('src', 'index.html');
+
+    // create a browser window
+    mainWindow = new BrowserWindow();
+
+    // load the html file
+    mainWindow.loadFile(htmlPath);
 });
+
